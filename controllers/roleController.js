b3d4434b1885cacf07788ce7122143a7ae539f38
TainSiddik <tainsiddik11@gmail.com>
2025-02-05 08:55:17 +0700

    created function edit role

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,11 +1,14 @@
 import Role from "../models/roleModel.js"
 
+// Get all data roles
 export const getAllRoles = async (req, res) => {
     try {
+        // Find all data role
         const roles = await Role.findAll()
         if (roles.length === 0) {
             return res.status(200).json({ message: "Empty Role" })
         }
+        // Get all data roles
         res.status(200).json(roles)
     } catch (error) {
         res.status(500).json({
@@ -16,6 +19,34 @@ export const getAllRoles = async (req, res) => {
     }
 }
 
+// Get one data role
+export const getOneRole = async (req, res) => {
+    try {
+        // Find role by id
+        const id = req.params.id
+        const role = await Role.findOne({
+            where: {
+                id: id
+            }
+        })
+        if (!role) return res.status(404).json({ message: "Role not found" })
+
+        // Get data role
+        res.status(200).json({
+            status: "Success",
+            message: "Get one user successfully",
+            data: role
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: "Error",
+            message: "Get all roles has failed",
+            error: error.message
+        })
+    }
+}
+
+// Add role
 export const addRole = async (req, res) => {
     try {
         const role = req.body.role
@@ -34,13 +65,60 @@ export const addRole = async (req, res) => {
         res.status(500).json({
             status: "Error",
             message: "Add role has failed",
-            data: add
+            error: error.message
         })
     }
 }
 
+// Edit role
+export const editRole = async (req, res) => {
+    try {
+        // Find role by id
+        const id = req.params.id;
+        const role = await Role.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (!role) return res.status(404).json({ message: "Role not found" });
+
+        const editRole = req.body.role
+
+        // check, what's the role names has used
+        const checkRole = await Role.findOne({
+            where: {
+                role: editRole
+            }
+        })
+        if (checkRole && checkRole.id !== role.id) {
+            return res.status(400).json({
+                status: "Failed",
+                message: "Role name has been used"
+            })
+        }
+
+        // Update role
+        const edit = await role.update({ role: editRole });
+        res.status(200).json({
+            status: "success",
+            message: "Edit role successfully",
+            data: edit
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "Error",
+            message: "Edit role failed",
+            error: error.message
+        });
+    }
+};
+
+
+// Delete role
 export const deleteRole = async (req, res) => {
     try {
+        // Find role by id
         const id = req.params.id
         const role = await Role.findOne({
             where: {
@@ -52,6 +130,8 @@ export const deleteRole = async (req, res) => {
         }
 
         const roleName = role.role
+
+        // Delete Role
         await role.destroy()
         res.status(200).json({
             status: "Success",
@@ -61,7 +141,8 @@ export const deleteRole = async (req, res) => {
     } catch (error) {
         res.status(500).json({
             status: "Error",
-            message: "Delete role has failed"
+            message: "Delete role has failed",
+            error: error.message
         })
     }
 }
